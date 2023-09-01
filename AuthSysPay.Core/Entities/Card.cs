fb68d847e7936bdada6e4a973c5c8c5e5dcd7edf
Raudo Moquete <raudo.moquete@rhodium.com.do>
2023-09-01 15:57:25 -0400

    card sec Admin

--- AuthSysPay.Core/Entities/Card.cs
+++ AuthSysPay.Core/Entities/Card.cs
@@ -1,7 +1,10 @@
-﻿namespace AuthSysPay.Core
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthSysPay.Core
 {
     public class Card : BaseEntity
     {
+        [MaxLength(15, ErrorMessage = "this field {0} needs 15 characters")]
         public string CardNumber { get; set; }
         public decimal Balance { get; set; }
     }
